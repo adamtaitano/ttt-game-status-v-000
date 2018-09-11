cbf10ef43fe6b590146119fcb404e1814700f24b
Adam Taitano <adam.taitano@gmail.com>
2018-09-11 19:07:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,6 +29,12 @@ def won?(board)
   #     false
   #   end
   # end
+  
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
 end
 
 def full?(board)
