1657de06eb6cc14bfa8708242fde1800665d6cd4
Adam Taitano <adam.taitano@gmail.com>
2018-09-11 18:41:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,6 +6,8 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
+winner_token = nil
+
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination| 
     win_index_1 = win_combination[0]
@@ -17,8 +19,10 @@ def won?(board)
     position_3 = board[win_index_3]
  
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      winner_token = "X"
       return win_combination # return the win_combination indexes that won.
     elsif position_1 == "O" && position_3 == "O" && position_3 == "O"
+      winner_token = "O"
       return win_combination
     else
       nil
@@ -34,4 +38,13 @@ end
 
 def draw?(board)
   full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  won?(board)
+  winner_token
 end
