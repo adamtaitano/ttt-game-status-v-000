0b611fd7e674d93e81a7db3fd597c904acac8b0f
Adam Taitano <adam.taitano@gmail.com>
2018-09-11 18:57:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,7 +9,9 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 winner_token = nil
 
 def won?(board)
-  empty = board.
+  empty = board.any? do |cell|
+    
+  end
   WIN_COMBINATIONS.each do |win_combination| 
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
